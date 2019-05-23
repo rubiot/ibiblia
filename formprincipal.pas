e476d3dbceb61e6bac16b042657266f8e1e5c0dc
Rubio Ribeiro Canario Terra <rubio.terra@gmail.com>
2019-05-22 22:48:40 -0300

    Fixing v1.0.0.57 crash once and for all (#59)
    Last fix didn't have any effect.

--- formprincipal.pas
+++ formprincipal.pas
@@ -847,7 +847,7 @@ begin
     if match.Success then
     begin
       SyncTheWordDict(match.Groups[1].Value); // morphology
-      if match.Groups.Count > 2 then
+      if not match.Groups[3].Value.IsEmpty then
         SyncTheWordDict(match.Groups[3].Value); // lemma
     end;
   end;
